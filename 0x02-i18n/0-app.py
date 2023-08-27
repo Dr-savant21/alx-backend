642d731622c3ffa6beca027443a3da7ff6664df3
Dr-savant21 <israelnosakhare21@gmail.com>
2023-08-27 18:29:54 +0100

    task 0x02-i8n added and done

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, redirect, url_for, request;
+
+app = Flask (__name__, template_folder="templates", static_folder="staticFolder")
+
+@app.route("/")
+def index():
+    return render_template("0-index.html")
+
+    
+if __name__ == "__main__":
+    app.run(debug=True)
